b592cd2be206edd9cf547f0de6863e46a93f84d0
victor-one <513044829@qq.com>
2022-07-01 00:14:19 +0800

    部署

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pocket_book',
+      script: 'pocket_book_server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '47.100.166.112',
+      ref: 'origin/main',
+      repo: 'git@github.com:victor-one/pocket_book.git',
+      path: '/workspace/pocket_book',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
