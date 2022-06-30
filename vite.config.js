43ce9928120975beeff99ec6569c77ddc97b6f1b
victor-one <513044829@qq.com>
2022-07-01 02:07:56 +0800

    修改指令

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
   plugins: [reactRefresh(), styleImport(
     {
       libs: [
